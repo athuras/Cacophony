e9dc750b192f34dfc3ac4c5b723ff951e9259fb9
Alexander Huras <alex.huras@gmail.com>
2014-07-03 23:56:30 -0400

    added proper typedefs for various sample formats

--- src/PortAudioTypes.jl
+++ src/PortAudioTypes.jl
@@ -6,18 +6,36 @@
 
 module PortAudioTypes
 
-export PaError, PaDeviceIndex, PaHostApiIndex, PaFloat32, PaStream, PaTime
+export PaError, PaDeviceIndex, PaHostApiIndex, PaStream, PaTime
 export PaSampleFormat, PaStreamFlags, PaStreamCallbackFlags
 
-abstract PaError <: Cint
-abstract PaDeviceIndex <: Cint
-abstract PaHostApiIndex <: Cint
-abstract paFloat32 <: Cfloat
-abstract PaStream <: Ptr{Void}
-abstract PaTime <: Cdouble
-abstract PaSampleFormat <: Culong
-abstract PaStreamFlags <: Culong
-abstract PaStreamCallbackFlags <: Culong
+typealias PaError               Cint
+typealias PaDeviceIndex         Cint
+typealias PaHostApiIndex        Cint
+typealias PaStream              Ptr{Void}
+typealias PaTime                Cdouble
+typealias PaStreamFlags         Culong
+typealias PaStreamCallbackFlags Culong
+
+
+export paFloat32, paInt32, paInt24, paInt16, paInt8, paUInt8
+export paCustomFormat, paNonInterleaved
+
+typealias PaSampleFormat        Culong
+paFloat32   = convert(PaSampleFormat, 0x00000001)
+paInt32     = convert(PaSampleFormat, 0x00000002)
+paInt24     = convert(PaSampleFormat, 0x00000004)
+paInt16     = convert(PaSampleFormat, 0x00000008)
+paInt8      = convert(PaSampleFormat, 0x00000010)
+paUInt8     = convert(PaSampleFormat, 0x00000020)
+paCustomFormat   = convert(PaSampleFormat, 0x00010000)
+paNonInterleaved = convert(PaSampleFormat, 0x80000000)
+
+
+export PortAudioError
+
+type PortAudioError <: Exception
+    errorCode::PaError
 end
 
 module PortAudioEnums
@@ -26,79 +44,81 @@ module PortAudioEnums
 # just treated as subtypes of Cint.
 export PaEnum, PaErrorCode, PaHostApiTypeId, PaStreamCallbackResult
 
-abstract PaEnum <: Cint
-abstract PaErrorCode <: PaEnum
-abstract PaHostApiTypeId <: PaEnum
-abstract PaStreamCallbackResult <: PaEnum
+typealias PaEnum                 Cint
+typealias PaErrorCode            PaEnum
+typealias PaHostApiTypeId        PaEnum
+typealias PaStreamCallbackResult PaEnum
 end
 
 module PortAudioStructs
 # I'm not entirely sure how to deal with structs,
 # everything I've found seems to just treat things as pointers
 # and do a bunch of unsafe_load(...) operations.
-import PortAudioTypes
-export PaHostApiInfo, PaHostErrorInfo, PaDeviceInfo
-export PaStreamParameters, PaStreamCallbackTimeInfo, PaStreamInfo
+using PortAudioTypes
+using PortAudioEnums
+
+export PaHostApiInfo, PaHostErrorInfo, PaDeviceInfo, PaStreamParameters
+export PaStreamCallbackTimeInfo, PaStreamInfo
 
 # Contains info about a particular host API
 type PaHostApiInfo
-    structVersion::Cint,
-    type_id::PaHostApiTypeId,
-    name::Ptr{Uint8},
-    deviceCount::Cint,
-    defaultInputDevice::PaDeviceIndex,
+    structVersion::Cint
+    type_id::PaHostApiTypeId
+    name::Ptr{Uint8}
+    deviceCount::Cint
+    defaultInputDevice::PaDeviceIndex
     defaultOutputDevice::PaDeviceIndex
 end
 
 # Used to return information about a host error condition
 type PaHostErrorInfo
-    hostApiType::PaHostApiTypeId,
-    errorCode::Clong,
+    hostApiType::PaHostApiTypeId
+    errorCode::Clong
     errorText::Ptr{Uint8}
 end
 
 # Info and capabilities of PortAudio devices:
 # Devices may input, output, or both,
 type PaDeviceInfo
-    structVersion::Cint,
-    name::Ptr{Uint8},
-    hostApi::PaHostApiIndex,
-    maxInputChannels::Cint,
-    maxOutputChannels::Cint,
+    structVersion::Cint
+    name::Ptr{Uint8}
+    hostApi::PaHostApiIndex
+    maxInputChannels::Cint
+    maxOutputChannels::Cint
 
     # Default latency values for interactive performance
-    defaultLowInputLatency::PaTime,
-    defaultLowOutputPatency::PaTime,
+    defaultLowInputLatency::PaTime
+    defaultLowOutputPatency::PaTime
 
     # Default latency for robust non-interactive applications
-    defaultHighInputLatency::PaTime,
-    defaultHighOutputLatency::Patime,
+    defaultHighInputLatency::PaTime
+    defaultHighOutputLatency::PaTime
 
     defaultSampleRate::Cdouble
 end
 
 type PaStreamParameters
-    device::PaDeviceIndex,
-    channelCount::Cint,
-    sampleFormat::PaSampleFormat,
-    suggestedLatency::PaTime,
+    device::PaDeviceIndex
+    channelCount::Cint
+    sampleFormat::PaSampleFormat
+    suggestedLatency::PaTime
     hostApiSpecificStreamInfo::Ptr{Void}
 end
 
 type PaStreamCallbackTimeInfo
     # The time when the first sample of the input buffer was captured at the ADC input
-    inputBufferAdcTime::PaTime,
+    inputBufferAdcTime::PaTime
     # The time when the stream callback was invoked
-    currentTime::PaTime,
+    currentTime::PaTime
     # The time when the first sample of the output buffer will output the DAC
     outputBufferDacTime::PaTime
 end
 
 type PaStreamInfo
-    structVersion::Cint,
+    structVersion::Cint
     # Times are expressed in SECONDS
-    inputLatency::PaTime,
-    outputLatency::Patime,
+    inputLatency::PaTime
+    outputLatency::PaTime
     sampleRate::Cdouble
 end
 end
