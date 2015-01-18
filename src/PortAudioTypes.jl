bac75e94bb0271f91b2ea35150283f114c4d2852
Alexander Huras <ahuras@twitter.com>
2015-01-17 18:10:06 -0800

    house cleaning

--- src/PortAudioTypes.jl
+++
@@ -1,8 +0,0 @@
-# Julia Wrapper of the types/structs/enums in portaudio.h
-module PortAudioTypes
-
-include("pa_NativeTypes.jl")
-include("pa_NativeEnums.jl")
-include("pa_NativeStructs.jl")
-
-end
--- src/pa_NativeEnums.jl
+++
@@ -1,7 +0,0 @@
-#  Mixin for PortAudioTypes.jl
-export PaEnum, PaErrorCode, PaHostApiTypeId, PaStreamCallbackResult
-
-typealias PaEnum                 Cint
-typealias PaErrorCode            PaEnum
-typealias PaHostApiTypeId        PaEnum
-typealias PaStreamCallbackResult PaEnum
