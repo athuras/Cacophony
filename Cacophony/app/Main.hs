05845365d1a9ef36138f247eb10c7fb7fc2a1220
Alexander Huras <alex.huras@gmail.com>
2017-03-19 18:55:06 -0700

    cleaned up, now lists all midi devices

--- Cacophony/app/Main.hs
+++ Cacophony/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Lib (showDevices)
 
 main :: IO ()
-main = showMidiDevices
+main = showDevices
--- Cacophony/src/Lib.hs
+++ Cacophony/src/Lib.hs
@@ -1,14 +1,32 @@
-module Lib
-    ( showMidiDevices
-    ) where
+module Lib (
+    getMidiDevices
+  , showDefaultDevice
+  , showDevices
+  ) where
 
 import Sound.PortMidi
 
-showMidiDevices :: IO ()
-showMidiDevices = do
+showDefaultDevice :: IO ()
+showDefaultDevice = do
   initialize
   Just deviceId <- getDefaultInputDeviceID
   dInfo <- getDeviceInfo deviceId
   print $ show dInfo
   terminate
   print "Done"
+
+
+getMidiDevices :: IO ([DeviceID])
+getMidiDevices = do
+  c <- countDevices
+  return $ map fromIntegral [0..(toInteger c)]
+
+showDevices :: IO ()
+showDevices = do
+  initialize
+  devices <- getMidiDevices
+  sequence_ (map (\d -> do
+    dInfo <- getDeviceInfo d
+    print $ show dInfo) devices)
+  err <- terminate
+  print $ show err
